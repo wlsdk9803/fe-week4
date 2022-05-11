a2ad79adb0e39add7f02b2fbfc9409ff8459cd79
adultlee <seoungin1228@gmail.com>
2022-05-12 01:53:04 +0900

    feat: 3번문제 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import InputSample from "./component/Input";
+import Problem from "./component/Problem";
+
 function App() {
-  return <>hihi</>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      {count}
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        gdgdgd
+      </button>
+      <InputSample />
+      <Problem />
+    </>
+  );
 }
 
 export default App;
---
+++ src/component/Problem/index.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+
+const ProblemWrapper = styled.button`
+  width: 100px;
+  height: 100px;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Problem = () => {
+  const [count, setCount] = useState(0);
+  const [isClick, setIsClick] = useState(false);
+
+  useEffect(() => {
+    setCount(count + 1);
+  }, [isClick]);
+
+  return (
+    <ProblemWrapper>
+      {count ? count : 0}
+      <button onClick={() => setIsClick(!isClick)}>button</button>
+    </ProblemWrapper>
+  );
+};
+
+export default Problem;
