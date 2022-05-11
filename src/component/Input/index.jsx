e8a096a7976f1fe103599a1e14788f44be4c28e4
adultlee <seoungin1228@gmail.com>
2022-05-12 02:34:50 +0900

    fix: 1번문제 주석

--- src/component/Input/index.jsx
+++ src/component/Input/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 
 const InputWrapper = styled.div`
@@ -17,13 +17,17 @@ const ViewWrapper = styled.div`
 `;
 
 function InputSample() {
-  // const [] = useState();
-
+  //이 페이지에서 useState를 단 한번만 사용한다면 선착순 한분에게 커피한잔 사드립니다!
+  // 요 위치에 useState 구문이 들어가면 됩니다!
   const onChange = (e) => {
-    console.log(e.target.value);
+    console.log(e.target.value); // e.target.value 가 무엇일까요??
+    // 여기에 코드를 작성하여 해결합니다!
+    // onChange 함수를 읽히고 이 함수가 사용될때 useState 가 사용되면 되겠죠?
   };
 
-  const onReset = () => {};
+  const onReset = () => {
+    // 여기는 reset 버튼을 눌렀을때 발생하는 함수입니다!!
+  };
 
   return (
     <div>
